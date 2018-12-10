2906795e7d850ce9e9a03f9697448f5e61ff22f6
TUT3Software4Project <tut.umemura.lab@gmail.com>
2018-12-10 13:33:14 +0900

    Interfaces: To be used for Javadoc

--- s4/specification/FrequencerInterface.java
+++ s4/specification/FrequencerInterface.java
@@ -1,14 +1,44 @@
 package s4.specification;
 
-public interface FrequencerInterface {     // This interface provides the design for frequency counter.
-    void setTarget(byte  target[]); // set the data to search.
-    void setSpace(byte  space[]);  // set the data to be searched target from.
-    int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
-                    //Otherwise, it return 0, when SPACE is not set or SPACE's length is zero
-                    //Otherwise, get the frequency of TAGET in SPACE
-    int subByteFrequency(int start, int end);
-    // get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
-    // For the incorrect value of START or END, the behavior is undefined.
-}
+/**
+ * This interface provides the design for a frequency counter.
+ */
+public interface FrequencerInterface {
+    /**
+     * Set the data to search.
+     *
+     * @param target the data to search
+     */
+    void setTarget(byte target[]);
 
+    /**
+     * Set the data to be searched target from.
+     *
+     * @param space the data to be searched target from
+     */
+    void setSpace(byte space[]);
+
+    /**
+     * Get the frequency count of TARGET.
+     *
+     * It returns -1 when TARGET is not set or TARGET's length is zero.
+     * It returns 0 when SPACE is not set or SPACE's length is zero.
+     * Otherwise, returns the frequency of TAGET in SPACE.
+     *
+     * @return frequency count
+     */
+    int frequency();
+
+    /**
+     * Get the frequency count of subBytes of TARGET.
+     *
+     * i.e. target[start], taget[start+1], ... , target[end-1].
+     * For the incorrect value of START or END, the behavior is undefined.
+     *
+     * @param start
+     * @param end
+     * @return frequency count in subBytes
+     */
+    int subByteFrequency(int start, int end);
+}
 
